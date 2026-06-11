885cabd78ce375eaa6edd6f61db556758f8f27d7
Orange <jiangjuncheng253@icloud.com>
2026-06-11 20:25:09 +0800

    chore(dev): 关闭默认性能浮层

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  Rack::MiniProfiler.config.enabled = ENV["RACK_MINI_PROFILER"] == "1"
   Rack::MiniProfiler.config.skip_paths = [ "/design-system", "/assets", "/cable", "/manifest", "/favicon.ico", "/hotwire-livereload", "/logo-pwa.png" ]
   Rack::MiniProfiler.config.max_traces_to_show = 50
 end
